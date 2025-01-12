da6af4fc74903e3a3ade037e030feb7f95e9fe1e
Psynian <132937418+Psynian@users.noreply.github.com>
2025-01-12 20:39:10 +0000

    first draft

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 
-st.title("🎈 My new app")
+URL = "https://medical.hee.nhs.uk/medical-training-recruitment/medical-specialty-training/equality-and-diversity/equality-and-diversity-2023-recruitment-data/country-of-qualification-2023-recruitment-data"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+tables = pd.read_html(str(soup))
+
+
+
+st.title("🎈 My new apppppp")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write(len(tables))
+
+st.dataframe(tables[0])
